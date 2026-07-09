771a4ef99c8eae95dadb942e6dbfa4f7ffd6b70f
yPabloo <pablo.afreire@gmail.com>
2026-07-08 23:05:16 -0300

    refactor: alteracao da senha admin hash

--- database/init/04_admin_seed.sql
+++ database/init/04_admin_seed.sql
@@ -1,5 +1,5 @@
 -- Seed administrativo (deploy completo)
--- Usuário: admin | Senha: admin123
+-- Usuário: admin | Senha: SMTT@2026
 
 INSERT INTO admin_users (username, nome, nome_completo, email, password_hash, status)
 VALUES (
@@ -7,7 +7,7 @@ VALUES (
     'Administrador',
     'Administrador do Sistema',
     'admin@smtt.local',
-    '$2y$10$IFAz/WwhO8JmgkWSefqLqOtCWwQoQnXYgp9Z73GR3nWZBMMaj9y3K',
+    '$2y$10$FwiFghQ2vahWCQ9wjVKteO4RF6l25sRFy9rkLgvpT5zT2q2BBJ27a',
     'ativo'
 )
 ON CONFLICT (username) DO NOTHING;
